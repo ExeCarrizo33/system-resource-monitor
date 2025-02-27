00f3c52e27612fe26bda2c484056c9ccdf5d0ad9
Exe <exelcarrizo@gmail.com>
2025-02-27 17:53:36 -0300

    Activacion de Shedule con la anotacion

--- monitor-service/src/main/java/com/monitor/monitor_service/MonitorServiceApplication.java
+++ monitor-service/src/main/java/com/monitor/monitor_service/MonitorServiceApplication.java
@@ -2,8 +2,10 @@ package com.monitor.monitor_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MonitorServiceApplication {
 
     public static void main(String[] args) {
